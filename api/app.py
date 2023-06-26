2e2a21e17ac82002f8f4ea811d5259c6f5a7dc9c
suhyeoonn <suhyeon1740@gmail.com>
2023-06-27 00:23:27 +0900

    트윗 글 올리기 API 추가

--- api/app.py
+++ api/app.py
@@ -8,6 +8,8 @@ def ping():
 
 app.users = {}
 app.id_count = 1
+app.tweets = []
+
 # 회원 가입 API
 @app.route("/sign-up", methods=['POST'])
 def sign_up():
@@ -17,3 +19,23 @@ def sign_up():
     app.id_count = app.id_count + 1
 
     return jsonify(new_user)
+
+# 트윗 글 올리기 API
+@app.route("/tweet", methods=['POST'])
+def tweet():
+    payload = request.json
+    user_id = int(payload['id'])
+    tweet = payload['tweet']
+
+    if user_id not in app.users:
+        return '사용자가 존재하지 않습니다', 400
+    
+    if len(tweet) > 300:
+        return '300자를 초과했습니다', 400
+    
+    app.tweets.append({
+        'user_id': user_id,
+        'tweet': tweet
+    })
+
+    return '', 200
