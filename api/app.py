a75a927dbc3b166301f134cf0db71c4b0a54503f
suhyeoonn <suhyeon1740@gmail.com>
2023-07-01 09:55:39 +0900

    인증 엔드포인트 구현

--- api/app.py
+++ api/app.py
@@ -1,5 +1,6 @@
 from datetime   import datetime, timedelta
-from flask import Flask, jsonify, request, current_app
+from functools import wraps
+from flask import Flask, jsonify, request, current_app, Response, g
 from flask.json import JSONEncoder
 from sqlalchemy import create_engine, text
 import bcrypt
@@ -104,6 +105,27 @@ def check_user(email):
             WHERE email = :email
         """), {'email': email}).fetchone()
 
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        access_token = request.headers.get('Authorization')
+        if access_token is not None:
+            try:
+                payload = jwt.decode(access_token, current_app.config['JWT_SECRET_KEY'], 'HS256')
+            except jwt.InvalidTokenError:
+                payload = None
+
+            if payload is None: return Response(status=401)
+
+            user_id = payload['user_id']
+            g.user_id = user_id
+            g.user = get_user(user_id) if user_id else None
+        else:
+            return Response(status = 401)
+        
+        return f(*args, **kwargs)
+    return decorated_function
+
 def create_app(test_config = None):
     app = Flask(__name__)
 
@@ -146,7 +168,6 @@ def create_app(test_config = None):
             }
 
             token = jwt.encode(payload, app.config['JWT_SECRET_KEY'], 'HS256')
-            print(token)
 
             return jsonify({
                 'access_token' : token
@@ -158,8 +179,10 @@ def create_app(test_config = None):
 
     # 트윗 글 올리기 API
     @app.route("/tweet", methods=['POST'])
+    @login_required
     def tweet():
         user_tweet = request.json
+        user_tweet['id'] = g.user_id
         tweet = user_tweet['tweet']
 
         if len(tweet) > 300:
@@ -171,6 +194,7 @@ def create_app(test_config = None):
 
     # 팔로우 API
     @app.route("/follow", methods=['POST'])
+    @login_required
     def follow():
         payload = request.json
         insert_follow(payload)
@@ -178,6 +202,7 @@ def create_app(test_config = None):
         return '', 200
 
     @app.route("/unfollow", methods=['POST'])
+    @login_required
     def unfollow():
         payload = request.json
         delete_follow(payload)
